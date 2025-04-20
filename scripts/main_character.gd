85d53dcc9e23375e3c341d490d44504a36f5daa2
Peter Januarius <pete@nexgencodecamp.com.au>
2025-04-20 20:21:10 +1000

    Stage 6 - Improved Movement

--- scripts/main_character.gd
+++ scripts/main_character.gd
@@ -1,14 +1,22 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 400.0
+const JUMP_VELOCITY = -900.0
+@onready var sprite_2d: AnimatedSprite2D = $Sprite2D
 
 
 func _physics_process(delta: float) -> void:
+	# Animations
+	if(velocity.x > 1 || velocity.x < -1):
+		sprite_2d.animation = "running"
+	else: 
+		sprite_2d.animation = "default"
+		
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		sprite_2d.animation = "jumping"
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -20,6 +28,9 @@ func _physics_process(delta: float) -> void:
 	if direction:
 		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, 12)
 
 	move_and_slide()
+	
+	var isLeft = velocity.x < 0
+	sprite_2d.flip_h = isLeft
